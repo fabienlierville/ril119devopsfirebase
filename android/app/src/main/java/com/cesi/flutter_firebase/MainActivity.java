0ea876b5d7aed06028144bd5462320f91749ed8a
Fabien <flierville@cesi.fr>
2021-05-12 14:22:09 +0200

    init

---
+++ android/app/src/main/java/com/cesi/flutter_firebase/MainActivity.java
@@ -0,0 +1,6 @@
+package com.cesi.flutter_firebase;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/pages/page_admob.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PageAdMob extends StatefulWidget {
+  const PageAdMob({Key key}) : super(key: key);
+
+  @override
+  _PageAdMobState createState() => _PageAdMobState();
+}
+
+class _PageAdMobState extends State<PageAdMob> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
